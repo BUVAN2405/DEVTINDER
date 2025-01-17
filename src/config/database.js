b34a7244b07d673d4be62c6bcd9280f4a4e58488
BHUVANESHWARAN <bhuvanesh020405@gmail.com>
2025-01-17 21:52:38 +0530

    Delete src/config directory

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://Buvan:vb180205@node.zg10q.mongodb.net/"
-  );
-};
-
-module.exports = connectDB;
